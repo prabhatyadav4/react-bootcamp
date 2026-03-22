c008cb09a65acba7f4c2f7f04ad2518e154875eb
Prabhat <osrprabhatyadav4@gmail.com>
2026-03-23 00:16:14 +0530

    BASIC: added two react apps

---
+++ 00_basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h4>This is Chai aur React Playlist</h4>
+    </>
+  )
+}
+
+export default App;
---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Chai Aur Code</h1>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
