5b8b8097f042cf09b73028e928adb67fe5aceb87
Prabhat <osrprabhatyadav4@gmail.com>
2026-03-24 23:40:36 +0530

    BASIC: added tailwind and props

---
+++ 04_tailwindProps/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='bg-black text-amber-50'>Tailwind Test</div>
+     <Card title = "Prabhat" description = "Clicked by Prabhat Yadav"/>
+     <Card title = "Mayank" description = "Clicked by Mayank Sharma"/>
+     <Card/>
+    </>
+  )
+}
+
+export default App
