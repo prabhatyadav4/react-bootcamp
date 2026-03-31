ff41ae837f4985645d29bb29f4617c1e7301e553
Prabhat <osrprabhatyadav4@gmail.com>
2026-03-31 22:06:14 +0530

    CHAIBLOG: feat env and config env

---
+++ 13_ChaiBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      <div>Hello ChaiBlog</div>
+    </>
+  )
+}
+
+export default App
