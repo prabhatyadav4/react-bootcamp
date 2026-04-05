ca2152859691086c783717bdb4bc2e0f935cb380
Prabhat <osrprabhatyadav4@gmail.com>
2026-04-05 23:23:08 +0530

    CHAIBLOG: feat component RTE

--- 13_ChaiBlog/src/components/index.js
+++ 13_ChaiBlog/src/components/index.js
@@ -1,7 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container";
+import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/logoutBtn";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
