7a5685b04ea4006f0312324b59a09a6cc6f042c4
Prabhat <osrprabhatyadav4@gmail.com>
2026-04-06 19:02:58 +0530

    CHAIBLOG: fix RTE api key

--- 13_ChaiBlog/src/components/RTE.jsx
+++ 13_ChaiBlog/src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
+import conf from '../conf/conf.js' 
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
@@ -12,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="cplu3pqso06cxw0q3n5s35n5irr7h03umme447zop8hfholg"
+            apiKey={conf.tinymceApiKey}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
--- 13_ChaiBlog/src/conf/conf.js
+++ 13_ChaiBlog/src/conf/conf.js
@@ -2,10 +2,9 @@ const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL).trim(),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID).trim(),
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID).trim(),
-  appwriteCollectionId: String(
-    import.meta.env.VITE_APPWRITE_COLLECTION_ID,
-  ).trim(),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID).trim(),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID).trim(),
+  tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY).trim(),
 };
 
 export default conf;
